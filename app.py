4db5226f80b62c88f0748e4c0dfe478ef75d05af
Arib Nawal <65786174+arib168@users.noreply.github.com>
2021-12-02 18:01:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ text_model = joblib.load('spam-ham')        #load the model using joblib
 ip = st.text_input('Enter your message :')  #user input in streamlit 
 op = text_model.predict([ip])               #predict if the written message is spam or ham 
 if st.button('Predict'):                    #create a button called as predict 
-  st.title(op[0])                           #if the button is pressed, then print if the msg is spam or ham 
+  st.title(op[0])                           #if the button is pressed, then print if the msg is spam or ham  
